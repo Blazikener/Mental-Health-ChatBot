3b873bc440af185b00cc52a2c8a05021775c77c4
Yasir <147266162+Blazikener@users.noreply.github.com>
2025-05-17 23:30:00 +0400

    Update database.py

--- database.py
+++ database.py
@@ -14,20 +14,20 @@ def get_db():
         db.close()
 
 # Checks for last 5 moods
-def get_latest_moods(userid: int, db: Session, n: int = 5):
+def get_latest_moods(user_id: int, db: Session, n: int = 5):
     from models import ChatHistory
     return (db.query(ChatHistory.mood)
-        .filter(ChatHistory.userid == userid)
+        .filter(ChatHistory.user_id == user_id)
         .order_by(ChatHistory.timestamp.desc())
         .limit(n)
         .all())
 
 # Pick the dominant mood from the past 5 moods
-def update_dominant_mood(userid: int, dominant_mood: str, db: Session):
+def update_dominant_mood(user_id: int, dominant_mood: str, db: Session):
     from models import UserProfile
-    profile = db.query(UserProfile).filter(UserProfile.userid == userid).first()
+    profile = db.query(UserProfile).filter(UserProfile.user_id == user_id).first()
     if not profile:
-        profile = UserProfile(userid=userid, dominant_mood=dominant_mood)
+        profile = UserProfile(user_id=user_id, dominant_mood=dominant_mood)
         db.add(profile)
     else:
         profile.dominant_mood = dominant_mood
